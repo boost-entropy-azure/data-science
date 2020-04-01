c22755d7909a01855d94c37af1470daa4bd8cc5e
Dino Tufekcic <dtufekcic@ctic-inc.com>
2020-04-01 16:46:39 -0700

    Populate kafka topics with kafka-python library

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@ import pytest
 import os
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
+from kafka import KafkaProducer
+from kafka.admin import KafkaAdminClient
 
 
 @pytest.fixture(scope='session')
@@ -11,7 +13,7 @@ def spark_context():
     os.environ['PYSPARK_SUBMIT_ARGS'] = \
         '--master local[4] --packages org.apache.spark:spark-streaming-kafka-0-8_2.11:2.4.5 pyspark-shell'
     sc = SparkContext()
-    sc.setLogLevel("WARN")
+    sc.setLogLevel('ERROR')
     yield sc
     sc.stop()
 
@@ -20,3 +22,18 @@ def spark_context():
 def streaming_context(spark_context):
     yield StreamingContext(spark_context, 1)
 
+
+@pytest.fixture(scope='function')
+def prime_kafka_pipeline():
+    _topic = ''
+
+    def _prime(topic, msg_count):
+        nonlocal _topic
+        _topic = topic
+        messenger = KafkaProducer(bootstrap_servers=['localhost:9092'])
+        for idx in range(msg_count):
+            messenger.send(topic=topic, value='{} message {}'.format(topic, idx).encode())
+
+    yield _prime
+    admin_client = KafkaAdminClient(bootstrap_servers='localhost:9092')
+    admin_client.delete_topics([_topic])
