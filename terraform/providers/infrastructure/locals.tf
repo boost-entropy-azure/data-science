532150b335435bc18da0e055dae13b3529d19efc
Matt Bangert <mbangert@ctic.us>
2021-01-29 20:13:12 -0700

    Formatted the failing terraform file.

--- terraform/providers/infrastructure/locals.tf
+++ terraform/providers/infrastructure/locals.tf
@@ -1,3 +1,3 @@
 locals {
-  remote_ips     = try(tolist(split(",", data.azurerm_key_vault_secret.azure_secret1.0.value)), [])
+  remote_ips = try(tolist(split(",", data.azurerm_key_vault_secret.azure_secret1.0.value)), [])
 }
