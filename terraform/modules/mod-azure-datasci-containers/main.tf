d1ca318bda7ee4e5d81444d60a2d6e0ee59a7977
christian.rowlands <christian.rowlands@gmail.com>
2021-02-13 13:54:06 -0500

    Update to version 0.1.5 of the MQTT to Azure Event Hub connector

--- terraform/modules/mod-azure-datasci-containers/main.tf
+++ terraform/modules/mod-azure-datasci-containers/main.tf
@@ -58,7 +58,7 @@ resource "azurerm_container_group" "datasci_mqtt" {
   # MQTT to Event Hub Connector
   container {
     name   = "connector"
-    image  = "chesapeaketechnology/mqtt-azure-event-hub-connector:0.1.4"
+    image  = "chesapeaketechnology/mqtt-azure-event-hub-connector:0.1.5"
     cpu    = "1"
     memory = "1.5"
 
