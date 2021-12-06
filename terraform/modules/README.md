28fbf208a9825df6257c6f9e6a9d2a843069d075
christian.rowlands <christian.rowlands@gmail.com>
2021-12-06 09:23:21 -0500

    Removed the 2nd postgres instance. Committing what I have for backup as it is still an untested WIP

--- terraform/modules/README.md
+++
@@ -1,9 +0,0 @@
-# terraform-datasci-modules
-
-This repository contains Terraform modules required by the Data Science efforts to deploy a pipeline.
-
-Current modules (in order of dependency):
-
-- mod-azure-datasci-eventhubs
-- mod-azure-datasci-nodes
-- mod-azure-datasci-fact-table
--- terraform/modules/mod-azure-datasci-grafana/modules/datasci-data/main.tf
+++ terraform/modules/mod-azure-datasci-grafana/modules/datasci-data/main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   #version = "~> 2.0"
   features {}
   disable_terraform_partner_id = true
-  skip_provider_registration = true
+  skip_provider_registration   = true
 }
 
 data "azurerm_resource_group" "data_resource_group" {
@@ -28,7 +28,7 @@ resource "azurerm_postgresql_server" "data-pg" {
   public_network_access_enabled    = true
   ssl_enforcement_enabled          = true
   ssl_minimal_tls_version_enforced = "TLS1_2"
-  tags = var.default_tags
+  tags                             = var.default_tags
 }
 
 resource "azurerm_postgresql_database" "grafana-db" {
@@ -39,8 +39,16 @@ resource "azurerm_postgresql_database" "grafana-db" {
   collation           = "English_United States.1252"
 }
 
+resource "azurerm_postgresql_database" "grafana-config-db" {
+  name                = "grafanaconfig"
+  resource_group_name = data.azurerm_resource_group.data_resource_group.name
+  server_name         = azurerm_postgresql_server.data-pg.name
+  charset             = "UTF8"
+  collation           = "English_United States.1252"
+}
+
 resource "azurerm_postgresql_firewall_rule" "data-fw" {
-  count = length(var.allow_access_from)
+  count               = length(var.allow_access_from)
   name                = join("-", ["dbfw", var.cluster_name, var.environment, "ds-data", count.index])
   resource_group_name = data.azurerm_resource_group.data_resource_group.name
   server_name         = azurerm_postgresql_server.data-pg.name
