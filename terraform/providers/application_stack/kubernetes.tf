5a1267b324e25c37ee064dc722b6212cae071a3c
Sean Hulse <shulse@ctic.us>
2022-09-14 13:20:04 -0700

    MONKEY-613: Move locals to resources for flux

--- terraform/providers/application_stack/kubernetes.tf
+++ terraform/providers/application_stack/kubernetes.tf
@@ -210,6 +210,12 @@ resource "kubectl_manifest" "install" {
 }
 
 locals {
+  # Convert documents list to include parsed yaml data
+  apply = [ for v in data.kubectl_file_documents.apply.documents : {
+      data: yamldecode(v)
+      content: v
+    }
+  ]
   install = [for v in data.kubectl_file_documents.install.documents : {
     data : yamldecode(v)
     content : v
